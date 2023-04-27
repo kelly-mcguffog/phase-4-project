f70a24e9c9f1ff80f858b22f7ce503547c194a6c
Kelly McGuffog <kellymcguffog@gmail.com>
2023-04-27 14:30:18 -0400

    validators

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,13 @@
 class Book < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
+    validates :title, :author, presence: true
+    validates :page_count, numericality: true
+
+    GENRE = ['Action & Adventure', 'Mystery', 'Thriller', 'Historical Fiction', 'Horror', 'Romance', 'Short Story', 'Memoir & Autobiography', 'Children']
+
+    validates :genre, inclusion: {
+    in: GENRE,
+    message: "Must be one of: #{GENRE.join(', ')}"
+  }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :books, through: :reviews
     has_secure_password
+    validates :username, uniqueness: true, on: :account_setup
+    validates :age, numericality: true, on: :account_setup
 end
