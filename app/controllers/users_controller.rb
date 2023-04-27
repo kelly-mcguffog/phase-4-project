32c12bbb8d657a28c70a7ed0abdc37f83ade94f3
Kelly McGuffog <kellymcguffog@gmail.com>
2023-04-27 12:29:23 -0400

    Log in/out/sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :user_data_invalid
+    rescue_from ActiveRecord::RecordNotFound, with: :user_unauthorized
+    
+    def create
+        new_user = User.create!(user_params)
+        session[:user_id] ||= new_user.id
+        render json: new_user, status: :created
+    end
+
+    def show
+        render json: User.find(session[:user_id]), status: :ok
+    end
+
+    private
+    
+    def user_params
+        params.permit(:name, :username, :password, :password_confirmation)
+    end
+
+    def user_data_invalid(error_hash)
+        render json: { errors: error_hash.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def user_unauthorized
+        render json: { error: "Please login to view your account page." }, status: :unauthorized
+    end
 end
