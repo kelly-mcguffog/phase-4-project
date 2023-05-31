6b03c21e1949d6aa81e9e18febc5d2fe8cc1d94b
Kelly McGuffog <kellymcguffog@gmail.com>
2023-05-31 06:57:59 -0400

    loading

--- client/src/Context/BookContext.js
+++ client/src/Context/BookContext.js
@@ -4,12 +4,15 @@ const BookContext = React.createContext();
 
 const BookProvider = ({ children }) => {
     const [books, setBooks] = useState([]);
+    const [loading, setLoading] = useState(true)
+
     useEffect(() => {
         fetch("/books")
         .then(r => r.json())
         .then(book => setBooks(book))
+        setLoading(false)
       }, []);
-  
+  if(loading) return <h1>Loading..</h1>
   return (
     <BookContext.Provider value={{books, setBooks}}>
         {children}
--- client/src/Context/UserContext.js
+++ client/src/Context/UserContext.js
@@ -4,15 +4,18 @@ const UserContext = React.createContext();
 
 const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true)
     
     useEffect(() => {
         fetch("/me").then((r) => {
           if (r.ok) {
             r.json().then((user) => setUser(user));
+            setLoading(false)
           }
         });
       }, []);
-
+  
+  if(loading) return <h1>Loading..</h1>
   return (
     <UserContext.Provider value={{user, setUser}}>
         {children}
